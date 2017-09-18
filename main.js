ae9f1ab7859d969fd6a88787ffb395b1d3d1e8a9
Cedric Dassy <cedric.dassy@ch.ibm.com>
2017-09-18 11:12:38 +0200

    Work in progree 1

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 "use strict";
 console.log("Running webapp...");
 
-let Express = require('express');
+let Express = require('express'); 
 let app = new Express();
 let port = process.env.PORT || 80;
+
 app.use(require('body-parser').urlencoded());
 app.use(require('body-parser').json());
 
 app.use(Express.static('./app'));
-app.get('/messages', (req, res) = > {
-    res.status(200).json(require('./messages.json'))
-})
-app.listen(port, () = > {
+
+
+app.get('/messages', function (req, res) {
+    res.status(200).json(require('./messages.json'));
+});
+
+app.listen(port, function () {
     //console.log(`Running webhook listener...`);
-    console.log(`App listening on port ${port}`);
+    console.log("App listening on port ${port}");
 })
 ;
